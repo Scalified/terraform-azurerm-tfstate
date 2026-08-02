512c7e57012e489d3e66337842926002e78d27f9
Vladyslav Baidak <vld.x@hotmail.com>
2026-08-02 19:49:23 +0300

    release: 0.0.3

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   storage_account_name   = coalesce(var.storage_account_name, "st${local.project}tfstate")
   tfstate_container_name = coalesce(var.tfstate_container_name, "tfstate")
   keyvault_name          = coalesce(var.keyvault_name, "kv-${local.project}")
-  allowed_ips            = var.allow_my_ip ? distinct(concat(var.allowed_ips, ["${trimspace(data.http.public_ip.response_body)}/32"])) : var.allowed_ips
+  allowed_ips            = var.allow_my_ip ? distinct(concat(var.allowed_ips, ["${trimspace(data.http.public_ip.response_body)}"])) : var.allowed_ips
   tags = merge(var.tags,
     {
       project    = local.project
